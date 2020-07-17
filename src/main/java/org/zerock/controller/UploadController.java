a3fb78a086723d51a2ec1c47c9e624bbffa807dd
alfer91 <dasungwkd@naver.com>
2020-07-17 16:13:47 +0900

    년/월/일 폴더의 생성

--- src/main/java/org/zerock/controller/UploadController.java
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -1,6 +1,8 @@
 package org.zerock.controller;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +15,17 @@ import lombok.extern.log4j.Log4j;
 @Controller
 @Log4j
 public class UploadController {
+	
+	private String getFolder() {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date = new Date();
+		
+		String str = sdf.format(date);
+		
+		return str.replace("-", File.separator);
+	}
 	
 	@GetMapping("/uploadForm")
 	public void uploadForm() {
@@ -51,6 +64,15 @@ public class UploadController {
 		
 		String uploadFolder = "C:\\upload";
 		
+		// make folder ...
+		File uploadPath = new File(uploadFolder, getFolder());
+		log.info("upload path: " + uploadPath);
+		
+		if(uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
+		// make yyyy/MM/dd folder
+		
 		for(MultipartFile multipartFile : uploadFile) {
 			
 			log.info("--------------------");
@@ -63,7 +85,8 @@ public class UploadController {
 			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);   // IE 파일 이름 처리
 			log.info("only file name: " + uploadFileName);
 			
-			File saveFile = new File(uploadFolder, uploadFileName);
+//			File saveFile = new File(uploadFolder, uploadFileName);
+			File saveFile = new File(uploadPath, uploadFileName);
 			
 			try {
 				multipartFile.transferTo(saveFile);
@@ -72,4 +95,5 @@ public class UploadController {
 			}
 		}
 	}
+	
 }
